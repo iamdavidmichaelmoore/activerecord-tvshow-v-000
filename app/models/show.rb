6c2c6617b6d242988876196891767861742b1c57
David Moore <hey@iamdavidmoore.com>
2018-07-07 14:09:11 +0000

    automatically backed up by learn

---
+++ app/models/show.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.find_by(rating: self.maximum(:rating))
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.find_by(rating: self.minimum(:rating))
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+end
